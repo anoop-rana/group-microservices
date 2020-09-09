8d2ff0e72bc297ce8b9ce62f5e604e256ff9ce88
Anoop Rana <anooprana45@gmail.com>
2020-09-09 23:46:57 +0530

    I18N implementation done. close #5

---
+++ group1-monolithic-rest-service/src/main/java/com/monolithic/config/AppConfiguration.java
@@ -0,0 +1,27 @@
+package com.monolithic.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class AppConfiguration {
+
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+}
--- group1-monolithic-rest-service/src/main/java/com/monolithic/controller/UserController.java
+++ group1-monolithic-rest-service/src/main/java/com/monolithic/controller/UserController.java
@@ -5,11 +5,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpHeaders;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -34,6 +37,14 @@ public class UserController {
 	
 	@Autowired
 	UserService service;
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	@GetMapping("/hi")
+	public String getInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
 
 	@GetMapping(path = {"","/all-users"})
 	public ResponseEntity<List<User>> getAllUsers() {
