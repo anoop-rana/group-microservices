73f109f750559bb374ac614ccf07a763d99a6e17
Anoop Rana <anooprana45@gmail.com>
2020-09-10 00:47:38 +0530

    override swagger according to your own org. close #7

--- group1-monolithic-rest-service/src/main/java/com/monolithic/config/SwaggerConfig.java
+++ group1-monolithic-rest-service/src/main/java/com/monolithic/config/SwaggerConfig.java
@@ -10,6 +10,9 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,15 +21,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("", "", "");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("MyTutorial org Api Documentation", "MyTutorial org",
+			"1.0", "urn:tos", DEFAULT_CONTACT, "MyTutorial.og", "http://www.MyTutorial.org/licenses/LICENSE-2.0",
+			new ArrayList<VendorExtension>());
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
 	}
-	
+
 	@Bean
-    public LinkDiscoverers discoverers() {
-        List<LinkDiscoverer> plugins = new ArrayList<>();
-        plugins.add(new CollectionJsonLinkDiscoverer());
-        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-    }
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+	}
 }
