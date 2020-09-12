76fe6793f4db12ccb79407a94a63eaccb3e3addd
Anoop Rana <anooprana45@gmail.com>
2020-09-12 17:48:02 +0530

    spring cloud config server with config file directory.

---
+++ group2-cab-config-server/src/test/java/com/config/Group2CabConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2CabConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
